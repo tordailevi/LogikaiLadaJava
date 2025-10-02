4b199e9c9c729e6a7f8d35c806b5445e79ebb684
tordai.l.barnabas <tordai.l.barnabas@dk.akkszalezi.hu>
2025-10-02 15:35:46 +0200

    Vezerlo Feladat metodus nelkul

--- src/vezerlo/LadaVezerlo.java
+++ src/vezerlo/LadaVezerlo.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vezerlo;
 
-/**
- *
- * @author viczai.p.milan
- */
+import modell.LadaModell;
+import nezet.LadaNezet;
+
 public class LadaVezerlo {
+
+    private LadaModell modell;
+    private LadaNezet nezet;
+           
     
+    public LadaVezerlo(LadaModell modell, LadaNezet nezet) {
+        this.modell = modell;
+        this.nezet = nezet;
+    }
+    
+    public void feladat(){
+        
+    }
 }
